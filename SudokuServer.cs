7edb81a570c53095e5aa145ea7fbeb5b16a799b2
viducthanhdat-web <viducthanhdat3@gmail.com>
2026-05-31 15:25:42 +0700

    Initial commit

---
+++ SudokuServer.cs
@@ -0,0 +1,15 @@
+namespace SudokuServer
+{
+    public partial class SudokuServer : Form
+    {
+        public SudokuServer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
