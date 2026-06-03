0c76f58e2740df4597ad2f026e4e913f75376893
vidat0508 <viducthanhdat3@gmail.com>
2026-06-03 17:16:38 +0700

    Refactor Player class to public and enhance properties
    Updated Player class to public and added properties for Username, Score, and IsReady linked to ClientSession.

--- SudokuServer/Models/Player.cs
+++ SudokuServer/Models/Player.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using SudokuServer.Network;
 
 namespace SudokuServer.Models
 {
-    internal class Player
+    public class Player
     {
+        public string Id { get; set; }
+        public string Username
+        {
+            get => Session.Username;
+            set => Session.Username = value;
+        }
+        public int Score
+        {
+            get => Session.Score;
+            set => Session.Score = value;
+        }
+        public bool IsReady
+        {
+            get => Session.IsReady;
+            set => Session.IsReady = value;
+        }
+        public string? RoomId { get; set; }
+        public ClientSession Session { get; }
+
+        public Player(string id, string username, ClientSession session)
+        {
+            Id = id;
+            Session = session;
+            Username = username;
+            Score = 0;
+            IsReady = false;
+            RoomId = null;
+        }
     }
 }
